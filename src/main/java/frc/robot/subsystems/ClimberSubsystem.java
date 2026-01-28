a720d1bd8a7c18ab6af2735379557186fb120b27
frc10553 <oregonhsrobotics@gmail.com>
2026-01-28 16:31:47 -0600

    created Climbersubsystem

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class ClimberSubsystem implements Subsystem {
+    private final TalonFX motor;
+    private final double defaultSpeed = 0.9;
+
+    // constructor
+    public ClimberSubsystem() {
+        motor = new TalonFX(30);
+        SmartDashboard.putNumber("Climber Speed", this.defaultSpeed);
+    }
+
+    public void startMotor() {
+        motor.set(SmartDashboard.getNumber("Climber Speed", this.defaultSpeed));
+    }
+
+    public void stopMotor() {
+        motor.set(0);
+    } 
+}
