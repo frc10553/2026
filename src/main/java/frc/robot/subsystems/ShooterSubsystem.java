fb1f21add47f0bab7b5f588fe92ce3994f10b6eb
rjdklp <rjdklp@proton.me>
2026-01-28 17:32:09 -0600

    Add shooter motors and hood

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,20 +6,26 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class ShooterSubsystem implements Subsystem {
-    private final TalonFX motor;
+    private final TalonFX leftShooterMotor;
+    private final TalonFX rightShooterMotor;
+    private final TalonFX hoodMotor;
     private final double defaultSpeed = 0.9;
 
     // constructor
     public ShooterSubsystem() {
-        motor = new TalonFX(30);
+        leftShooterMotor = new TalonFX(100);
+        rightShooterMotor = new TalonFX(101);
+        hoodMotor = new TalonFX(102);
         SmartDashboard.putNumber("Shooter Speed", this.defaultSpeed);
     }
 
     public void startMotor() {
-        motor.set(SmartDashboard.getNumber("Shooter Speed", this.defaultSpeed));
+        leftShooterMotor.set(SmartDashboard.getNumber("Shooter Speed", this.defaultSpeed));
+        rightShooterMotor.set(SmartDashboard.getNumber("Shooter Speed", this.defaultSpeed));
     }
 
     public void stopMotor() {
-        motor.set(0);
+        leftShooterMotor.set(0);
+        rightShooterMotor.set(0);
     } 
 }
